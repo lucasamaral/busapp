9f0acdabb924e41d2ffbf7682f8088d85ea5035d
Lucas Amaral <lucasamaral1@gmail.com>
2014-06-01 19:06:26 -0300

    adding listview to which will display results

--- BusApp/src/main/java/com/urban/busapp/app/SearchStopFragment.java
+++ BusApp/src/main/java/com/urban/busapp/app/SearchStopFragment.java
@@ -5,13 +5,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.SearchView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class SearchStopFragment extends Fragment {
+
+    protected String[] stops = {"Av. Cristiano Machado 2000", "Praça da Estação", "Av. Afonso Pena 1409"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_stop, container, false);
+        ListAdapter adapter = new ArrayAdapter<String>(rootView.getContext(),
+                android.R.layout.simple_list_item_1, stops);
+        ListView stopList = (ListView) rootView.findViewById(R.id.listViewStop);
+        stopList.setAdapter(adapter);
         return rootView;
     }
 }
