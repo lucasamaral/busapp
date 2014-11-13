45c551eccd7fb6348b74a2791685fc55e02245bf
Lucas Amaral <lucasamaral1@gmail.com>
2014-11-13 04:43:05 -0200

    making StopPoint parcelable

--- BusApp/src/main/java/com/urban/busapp/app/models/StopPoint.java
+++ BusApp/src/main/java/com/urban/busapp/app/models/StopPoint.java
@@ -1,9 +1,12 @@
 package com.urban.busapp.app.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.android.gms.maps.model.LatLng;
 
 
-public class StopPoint {
+public class StopPoint implements Parcelable {
     private LatLng point;
     private Integer segId;
 
@@ -19,4 +22,38 @@ public class StopPoint {
     public Integer getSegId() {
         return segId;
     }
+
+    protected StopPoint(Parcel in) {
+        point = (LatLng) in.readValue(LatLng.class.getClassLoader());
+        segId = in.readByte() == 0x00 ? null : in.readInt();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(point);
+        if (segId == null) {
+            dest.writeByte((byte) (0x00));
+        } else {
+            dest.writeByte((byte) (0x01));
+            dest.writeInt(segId);
+        }
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<StopPoint> CREATOR = new Parcelable.Creator<StopPoint>() {
+        @Override
+        public StopPoint createFromParcel(Parcel in) {
+            return new StopPoint(in);
+        }
+
+        @Override
+        public StopPoint[] newArray(int size) {
+            return new StopPoint[size];
+        }
+    };
 }
